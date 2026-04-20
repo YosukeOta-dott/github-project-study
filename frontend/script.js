2d066cb9b3b4637a4b917684ba0634aa175f0bfe
oks-knagasaki <knagasaki@okaya-system.co.jp>
2026-04-20 15:19:33 +0900

    （実験）敢えてエラーが発生するコードを追加

--- frontend/script.js
+++ frontend/script.js
@@ -50,6 +50,8 @@ document.addEventListener("DOMContentLoaded", () => {
       } else {
         // Issue 4: 429 Too Many Requestsの場合、エラーメッセージ「連続した送信が確認されました。少し時間を置き、再度送信してください。」と表示する
         if (res.status === 429) {
+          const a = 1;
+          const b = a+1;
           responseMsg.textContent =
             "連続した送信が確認されました。少し時間を置き、再度送信してください。";
         } else {
